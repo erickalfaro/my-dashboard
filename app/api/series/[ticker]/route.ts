b86587f50a96224624ed153daeda89a99851c735
Erick <ericks.network@gmail.com>
2025-02-20 00:35:56 -0500

    test

--- app/api/series/[ticker]/route.ts
+++ app/api/series/[ticker]/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { ticker: string } }
 ) {
   const ticker = params.ticker;
